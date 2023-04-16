af34542d7f6a060c3e9430e60ee98d657cbe6382
Antoine Nd <43643450+entonoire@users.noreply.github.com>
2023-04-16 14:34:04 +0200

    Add files via upload

---
+++ identrm.py
@@ -0,0 +1,53 @@
+import os
+
+labomep = True
+
+calcul = input("\nWrite your calculations here: ")
+
+option1 = "(a)²+2*a*b+(b)²"
+option1_labomep = "(a)²+2\\times a\\times b+(b)²"
+
+option2 = "(a)²-2*a*b+(b)²"
+option2_labomep = "(a)²-2\\times a\\times b+(b)²"
+
+
+def addToClipBoard(text): os.system(f"echo {text} | clip")
+
+
+#(a+b)²
+if "+" in calcul and not "-" in calcul:
+	clean = calcul.replace("(", "").replace(")", "").replace("²", "").split("+")
+	a = clean[0]
+	b = clean[1]
+
+	
+	result = option1.replace("a", a).replace("b", b)
+
+	if labomep:
+		result_labomep = option1_labomep.replace("a", a).replace("b", b)
+		print(f"\nresult: {result}\nlabomep (copied in clipboard): {result_labomep}\n")
+		addToClipBoard(result_labomep)
+	else:
+		print(f"\nresult: {result}\n")
+
+
+#(a-b)²
+if "-" in calcul and not "+" in calcul:
+	clean = calcul.replace("(", "").replace(")", "").replace("²", "").split("-")
+	a = clean[0]
+	b = clean[1]
+
+	
+	result = option2.replace("a", a).replace("b", b)
+
+	if labomep:
+		result_labomep = option2_labomep.replace("a", a).replace("b", b)
+		print(f"\nresult: {result}\nlabomep (copied in clipboard): {result_labomep}\n")
+		addToClipBoard(result_labomep)
+	else:
+		print(f"\nresult: {result}\n")
+
+
+#(a-b)(a+b)
+if "+" in calcul and "-" in calcul:
+	print("option 3")
